37d30946fe3c964f7d9eca0f460366bb7e19324a
Jenni Malinen <s3maje01@utu.fi>
2025-01-20 18:34:25 +0200

    Siirretty tiedostot docs-kansioon

---
+++ docs/script1.js
@@ -0,0 +1,7 @@
+document.getElementById("nav02").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='EnJenniMalinen.html'>Front page</a></li>" +
+"<li><a href='EnCV.html'>CV</a></li>" +
+"<li><a href='EnPortfolio.html'>Portfolio</a></li>" +
+"<li><a href='EnYhteydenotto.html'>Contact</a></li>" +
+"</ul>";
