0a2f93d2db5b63d34003b32c08a5e305ff7ac610
Jenni Malinen <s3maje01@utu.fi>
2025-02-17 18:52:17 +0200

    Päivitetty docs-kansion tiedostot

--- script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("nav01").innerHTML =
-"<ul id='menu'>" +
-"<li><a href='index.html'>Etusivu</a></li>" +
-"<li><a href='CV.html'>CV</a></li>" +
-"<li><a href='Portfolio.html'>Portfolio</a></li>" +
-"<li><a href='Yhteydenotto.html'>Yhteydenotto</a></li>" +
-"</ul>";
